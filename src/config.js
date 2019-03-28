3a73e64abf24001576fcc5a1d1fd75060576853a
Jay Sabherwal <jaysabherwal@gmail.com>
2019-03-28 15:25:24 +0000

    Remove values from config.js

--- src/config.js
+++ src/config.js
@@ -4,8 +4,6 @@ module.exports = {
         consumer_secret:      process.env.CONSUMER_SECRET || '. . .',
         access_token:         process.env.ACCESS_TOKEN || '. . .',
         access_token_secret:  process.env.ACCESS_TOKEN_SECRET || '. . .',
-        timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-        strictSSL:            true,     // optional - requires SSL certificates to be valid.
     },
 
     query: process.env.QUERY || ''
